823ea053f95a6c201b63cd17cc0b8a72a5e326d9
PBscoots <vanderpool.andrew@gmail.com>
2017-08-12 02:09:55 +0200

    Added Javascript
    Added jQuery bits to handle the carousel and the peeping navbar

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,35 @@
-// This is a comment
+// carousel functionality
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows:false,
+    draggable:false,
+    pauseOnHover:false,
+    pauseOnFocus:false
+  });
+//peeping navbar
+
+  var $scrolled = 0;
+  $(window).scroll(function(){
+    var $scrollT = $(this).scrollTop();
+    if ($scrollT - $scrolled > 50){
+      var $navHeight = $(".navbar").css('height');
+      $(".navbar").animate({
+          top: '-' + $navHeight
+      },150);
+      $scrolled = $scrollT;
+    } else if ($scrolled - $scrollT > 50) {
+      $(".navbar").animate({
+          top: '0px'
+      },150);
+      $scrolled = $scrollT;
+    };
+    console.log($scrollT);
+    console.log($navHeight);
+  });
+
+
+});
